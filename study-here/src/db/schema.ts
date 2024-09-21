3ab15ab6e4394f33726c0bb5b424c82fb511ff3e
LegolasArkantos <136463001+LegolasArkantos@users.noreply.github.com>
2024-09-21 21:51:37 +0500

    Database (#15)
    * Kan 16 add images that will attract childen (#4)
    
    * test commit
    
    * new pic
    
    * pic fix
    
    * pic fix again
    
    * fixed format and pic
    
    * added stripe payment
    
    * added plan options
    
    * skill issue in css
    
    * [KAN-37][KAN-38][KAN-39] Implemented stripe payment adn products (#6)
    
    * chore: Add utility function for merging CSS class names
    
    * chore: Remove unused imports and update dependencies
    
    * chore: Update .gitignore and next.config.mjs, add new files and remove unused files
    
    * made ui better
    
    * better name hib
    
    * made mobile friendly
    
    * ui fix desktop
    
    * added navbar
    
    * changes
    
    * chore: Refactor layout and navbar components
    
    * refactor: Remove redundant heading in products page
    
    * remove empty file
    
    * chore: Add Suspense component for lazy loading in CheckoutPage
    
    * feat: Add LoadingSpinner component for displaying loading state
    
    ---------
    
    Co-authored-by: LegolasArkantos <unasaamir101@gmail.com>
    
    * added about page
    
    * Added svg for gmail and linkedin
    
    * edited about page
    
    * add new linkdin link
    
    * remove old payment button
    
    * here come tables
    
    ---------
    
    Co-authored-by: Ibrahim Iqbal <63118347+dIB59@users.noreply.github.com>

---
+++ study-here/src/db/schema.ts
@@ -0,0 +1,20 @@
+import { serial, text, pgTable, integer, uniqueIndex } from 'drizzle-orm/pg-core';
+
+export const students = pgTable('students', {
+  id: serial('id').primaryKey(),
+  name: text('name'),
+  email: text('email').unique(), 
+});
+
+export const courses = pgTable('courses', {
+    id: serial('id').primaryKey(),
+    courseName: text('course_name'),
+    courseCode: text('course_code').unique(), 
+  });
+
+export const enrollments = pgTable('enrollments', {
+studentId: integer('student_id').references(() => students.id),
+courseId: integer('course_id').references(() => courses.id),
+}, (enrollments) => ({
+uniqueEnrollment: uniqueIndex('unique_enrollment').on(enrollments.studentId, enrollments.courseId) 
+}));
